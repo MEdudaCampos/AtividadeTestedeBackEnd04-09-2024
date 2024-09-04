92413ec6db1548ba7322445f050e912cd6fe8338
Duda <eduardacampos652@gmail.com>
2024-09-04 12:29:13 -0300

    first commit

---
+++ index.test.js
@@ -0,0 +1,15 @@
+import { filtrarNumerosPares, ordemAlfabetica } from "./funcao";
+
+test('filtra apenas os números pares de uma lista', () => {
+  const numeros = [1, 2, 3, 4, 5, 6];
+  const resultadoEsperado = [2, 4, 6];
+  expect(filtrarNumerosPares(numeros)).toEqual(resultadoEsperado);
+});
+
+test('ordena a lista em ordem alfabetica', () => {
+  const listaOriginal = ["Raquel", "Duda", "Deyse"];
+  const listaEsperada = ["Deyse", "Duda", "Raquel"];
+
+  expect(ordemAlfabetica(listaOriginal)).toEqual(listaEsperada)
+})
+
